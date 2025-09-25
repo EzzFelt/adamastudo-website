394b85178683b68f82c9cdcea2ecf27096c63cb7
Enzo Feltrin <enzobrexo@gmail.com>
2025-09-24 23:25:34 -0300

    Created footer component

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+export const Footer = () => {
+    return(
+      <div>
+        <footer className="w-full h-20 bg-gray-800 flex justify-center items-center mt-20">
+          <p className="text-white">© 2025 Adamas Tudo. Todos os direitos reservados.</p>
+        </footer>
+      </div>
+    );
+}
