738377ba8a9580b090761b26683ff8f53edce3f5
Enzo Feltrin <enzobrexo@gmail.com>
2025-11-07 19:58:07 -0300

    updated the routes.ts for the new pages

--- app/routes.ts
+++ app/routes.ts
@@ -2,8 +2,17 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("/login", "routes/login.tsx"),
-  route("/signup", "routes/signup.tsx"),
-  route("/cronogram", "routes/cronogram.tsx"),
-  route("/service", "routes/service.tsx"),
+  route("login", "routes/login.tsx"),
+  route("signup", "routes/signup.tsx"),
+  
+
+  route("service", "routes/service.tsx"),
+  route("service/:id", "routes/service-detail.tsx"),        
+  route("service/:id/hire", "routes/service-hire.tsx"),      
+  route("service/add", "routes/service-add.tsx"),            
+  
+  route("my-services", "routes/my-services.tsx"),
+
+  route("cronogram", "routes/cronogram.tsx"),
+  route("history", "routes/history.tsx"),
 ] satisfies RouteConfig;
